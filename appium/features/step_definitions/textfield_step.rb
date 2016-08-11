dc4420ea6478e47870ffcbaf278999ce4abfc15d
sreepad <sreepadbhagwat@users.noreply.github.com>
2016-08-11 11:33:02 -0400

    adding file textfield_step.rb and initial commit

--- appium/features/step_definitions/textfield_step.rb
+++ appium/features/step_definitions/textfield_step.rb
@@ -1,4 +1,3 @@
-
 When(/^I verify the existence of the text filed return value should be true$/) do
   puts "Checking for the existence of button using exists method. Should be true.  --  #{on(Screen).username_exists?}"
   fail unless on(Screen).username_exists?
