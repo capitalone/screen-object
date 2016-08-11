ebb73218dcf7b65e8e5cb40e4b3b7766a003cc05
sreepad <sreepadbhagwat@users.noreply.github.com>
2016-08-11 11:44:43 -0400

    adding file checkbox.rb and initial commit

--- appium/lib/screen-object/accessors/checkbox.rb
+++ appium/lib/screen-object/accessors/checkbox.rb
@@ -12,6 +12,7 @@ See the License for the specific language governing permissions and limitations 
 ***********************************************************************************************************
 =end
 
+
 module ScreenObject
   module AppElements
     class CheckBox < AppElements::Element
@@ -32,8 +33,6 @@ module ScreenObject
         element.click if checked?
       end
       
-       
-
     end
   end
 end
