a375ef3ee67e66f856718ecc1d66fc3836cf720d
sreepad <sreepadbhagwat@users.noreply.github.com>
2016-08-11 11:39:13 -0400

    adding file env.rb and initial commit

--- appium/features/support/env.rb
+++ appium/features/support/env.rb
@@ -7,12 +7,10 @@ require 'screen-object'
 require_relative 'screen'
 require_rel 'screens'
 
-
 appium_txt = IO.readlines(File.dirname(File.expand_path('./', __FILE__)) + "/appium.txt")
 path = appium_txt[3].split(34.chr)
 $AppPath = path[1]
 Appium::Driver.new(Appium.load_appium_txt file: File.expand_path('./', __FILE__), verbose: true)
-
 
 Before {
   ScreenObject::Load_App.start_driver
