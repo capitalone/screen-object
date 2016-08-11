e33fec12ee8d51cc2e2925f9e09040a22531dc4a
sreepad <sreepadbhagwat@users.noreply.github.com>
2016-08-11 11:31:31 -0400

    adding file checkbox_step.rb and initial commit

--- appium/features/step_definitions/checkbox_step.rb
+++ appium/features/step_definitions/checkbox_step.rb
@@ -2,6 +2,7 @@ When(/^I verify the existence of the checkbox$/) do
  puts "Checking for the existence of button using exists method. Should be true.  --  #{on(Screen).remember_me?}"
 end
 
+
 Then(/^I should see the return value as true on checkbox existence$/) do
   fail unless on(Screen).remember_me?
   puts on(Screen).remember_me_checked?
