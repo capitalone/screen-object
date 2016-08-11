581d103262f6286a61cdf75cbe966dcdf498de5f
sreepad <sreepadbhagwat@users.noreply.github.com>
2016-08-11 11:48:42 -0400

    adding file elements.rb and initial commit

--- appium/lib/screen-object/elements.rb
+++ appium/lib/screen-object/elements.rb
@@ -11,6 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License. 
 ***********************************************************************************************************
 =end
+
 require 'screen-object/accessors/element'
 require 'screen-object/accessors/button'
 require 'screen-object/accessors/image'
