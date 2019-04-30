f130cef9028b3babff41f3ff37b8f75e76a14445
Ben Jerome <bjerome43@massmutual.com>
2019-04-30 19:54:22 -0400

    added new functions to accessors element.rb

--- appium/lib/screen-object.rb
+++ appium/lib/screen-object.rb
@@ -54,8 +54,10 @@ module ScreenObject
   end
 
   def wait_until(timeout = 5, message = nil, &block)
+    default_wait = driver.default_wait
     wait = Selenium::WebDriver::Wait.new(timeout: timeout, message: message)
     wait.until &block
+    driver.set_wait(default_wait)
   end
 
   def wait_step(timeout = 5, message = nil, &block)
@@ -66,84 +68,122 @@ module ScreenObject
   end
 
   def enter
-    #pending implementation
+    driver.send_keys(:enter)
   end
 
-  def scroll_down_find(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollStart,:end_x =>0,:end_y =>scrollEnd,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
+  def gesture(arg)
+    Appium::TouchAction.new(driver).swipe(start_x: arg[0], start_y: arg[1], end_x: arg[2], end_y: arg[3], duration: arg[4]).perform
+  rescue RuntimeError => e
+    raise("Error during gesture \n Error Details: #{e}")
   end
 
-  def scroll_down_click(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          driver.find_element(locator,locator_value).click
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollStart,:end_x =>0,:end_y =>scrollEnd,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
+  def scroll(direction = :down, duration = 1000)
+    size = driver.window_size
+    x = size.width / 2
+    y = size.height / 2
+    loc = case direction
+          when :up then    [x, y * 0.5, x, (y + (y * 0.3)), duration]
+          when :down then  [x, y, x, y * 0.5, duration]
+          when :left then  [x * 0.6, y, x * 0.3, y, duration]
+          when :right then [x * 0.3, y, x * 0.6, y, duration]
+          else
+            raise('Only up, down, left and right scrolling is supported')
+          end
+    gesture(loc)
   end
 
-  def scroll_up_find(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollEnd,:end_x =>0,:end_y =>scrollStart,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
+  def scroll_down
+    scroll(:down)
   end
 
-
-  def scroll_up_click(locator,locator_value,num_loop = 15)
-    scr = driver.window_size
-    screenHeightStart = (scr.height) * 0.5
-    scrollStart = screenHeightStart.to_i
-    screenHeightEnd = (scr.height) * 0.2
-    scrollEnd = screenHeightEnd.to_i
-    for i in 0..num_loop
-      begin
-        if (driver.find_element(locator,locator_value).displayed?)
-          driver.find_element(locator,locator_value).click
-          break
-        end
-      rescue
-        driver.swipe(:start_x => 0,:start_y => scrollEnd,:end_x =>0,:end_y =>scrollStart,:touchCount => 2,:duration => 0)
-        false
-      end
-    end
+  def scroll_up
+    scroll(:up)
   end
 
+  def swipe_left
+    scroll(:left)
+  end
+
+  def swipe_right
+    scroll(:right)
+  end
+
+  def swipe_element(locator, direction = :down, duration = 1000)
+    my_element = driver.find_element(locator.locator.first, locator.locator.last).rect
+    start_x = my_element.x
+    end_x = my_element.x + my_element.width
+    start_y = my_element.y
+    end_y = my_element.y + my_element.height
+    height = my_element.height
+    loc = case direction
+          when :up then [end_x * 0.5, (start_y + (height * 0.2)), end_x * 0.5, (end_y - (height * 0.2)), duration]
+          when :down then  [end_x * 0.5, (end_y - (height * 0.2)), start_x * 0.5, (start_y + (height * 0.3)), duration]
+          when :left then  [end_x * 0.9,  end_y - (height / 2), start_x, end_y - (height / 2), duration]
+          when :right then [end_x * 0.1, end_y - (height / 2), end_x * 0.9, end_y - (height / 2), duration]
+          else
+            raise('Only up, down, left and right scrolling is supported')
+          end
+    gesture(loc)
+  end
+
+  # Scroll Down in a direction until a string that matches is found,
+  #
+  # @param text      [String] The text you are looking for on the screen
+  def scroll_down_to_text(text)
+    scroll_text_to_view(text)
+  end
+
+  # Scroll Up in a direction until a string that matches is found,
+  #
+  # @param text      [String] The text you are looking for on the screen
+  def scroll_up_to_text(text)
+    scroll_text_to_view(text, :up)
+  end
+
+  # Scrolls in a direction until a string that matches is found,
+  #
+  # @param text      [String] The text you are looking for on the screen
+  # @param direction [symbol] The direction to search for an string
+  # @param timeout   [Integer] The amount of times in seconds we want to scroll to find the element
+  # @return          [Boolean]
+  def scroll_text_to_view(text, direction = :down, timeout = 40)
+    wait_until(timeout,'Unable to find element',&->{text_visible?(text, direction)})
+    scroll(direction)
+  end
+
+  # Scrolls in a direction if a text that matches is not found. return false,  otherwise return true
+  #
+  # @param text   [String] The text you are looking for on the screen
+  # @param direction [symbol] The direction to search for an string
+  # @return          [Boolean]
+  def text_visible?(text, direction)
+    driver.find(text).displayed?
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    scroll(direction)
+    false
+  end
+
+  # Scrolls in a direction if an element that matches is not found. return false,  otherwise return true
+  #
+  # @param element   [Element] The text you are looking for on the screen
+  # @param direction [symbol] The direction to search for an string
+  # @return          [Boolean]
+  def element_visible?(element, direction = :down)
+    driver.find_element("#{element.locator[0]}", "#{element.locator[1]}").displayed?
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    scroll(direction)
+    false
+  end
+
+  # Scrolls in a direction until an element that matches is found,  or return false
+  #
+  # @param element   [Element] The text you are looking for on the screen
+  # @param direction [symbol] The direction to search for an string
+  # @return          [Boolean]
+  #example: wait_until(timeout,'Unable to find element',&->{element_visible?(element, direction)})
+  def scroll_element_to_view(element, direction, time_out)
+    wait_until(time_out,'Unable to find element',&->{element_visible?(element, direction)})
+  end
 
   def drag_and_drop_element(source_locator,source_locator_value,target_locator,target_locator_value)
     l_draggable = driver.find_element(source_locator,source_locator_value)
--- appium/lib/screen-object/accessors/element.rb
+++ appium/lib/screen-object/accessors/element.rb
@@ -64,6 +64,20 @@ module ScreenObject
         %w[name resource-id value text]
       end
 
+      def get_position
+        my_element = element.rect
+        {
+            start_x: my_element.x,
+            end_x: my_element.x + my_element.width,
+            start_y: my_element.y,
+            end_y: my_element.y + my_element.height,
+            height: my_element.height,
+            width: my_element.width
+        }
+      rescue RuntimeError => err
+        raise("Error Details: #{err}")
+      end
+
       def dynamic_xpath(text)
         concat_attribute=[]
         element_attributes.each{|i| concat_attribute << %Q(contains(@#{i}, '#{text}'))}
@@ -81,9 +95,75 @@ module ScreenObject
         end
       end
 
-      def scroll
-        $driver.execute_script 'mobile: scrollTo',:element => element.ref
-        # $driver.execute_script("mobile: scroll",:direction => direction.downcase, :element => element.ref)
+      def gesture(args)
+        Appium::TouchAction.new($driver).swipe(start_x: args[0], start_y: args[1], end_x: args[2], end_y: args[3], duration: args[4]).perform
+      rescue RuntimeError => e
+        raise("Error during gesture in element.rb\n Error Details: #{e}")
+      end
+
+      def scroll(direction = :down, duration = 1000)
+        size = driver.window_size
+        x = size.width / 2
+        y = size.height / 2
+        loc = (case direction
+               when :up then    [x, y * 0.5, x, (y + (y * 0.5)), duration]
+               when :down then  [x, y, x, y * 0.5, duration]
+               when :left then  [x * 0.6, y, x * 0.3, y, duration]
+               when :right then [x * 0.3, y, x * 0.6, y, duration]
+               else
+                 raise('Only upwards, downwards, leftwards and rightwards scrolling are supported')
+               end)
+        gesture(loc)
+      end
+
+      def scroll_down
+        scroll(:down)
+      end
+
+      def scroll_up
+        scroll(:up)
+      end
+
+      def swipe_left
+        scroll(:left)
+      end
+
+      def swipe_right
+        scroll(:right)
+      end
+
+      def swipe_my_element(direction = :down, duration = 1000)
+        my_element = element.rect
+        start_x = my_element.x
+        end_x = my_element.x + my_element.width
+        start_y = my_element.y
+        end_y = my_element.y + my_element.height
+        height = my_element.height
+        loc = case direction
+              when :up then    [end_x * 0.5, (start_y + (height * 0.2)), end_x * 0.5, (end_y - (height * 0.2)), duration]
+              when :down then  [end_x * 0.5, (end_y - (height * 0.2)), start_x * 0.5, (start_y + (height * 0.3)), duration]
+              when :left then  [end_x * 0.9,  end_y - (height / 2), start_x, end_y - (height / 2), duration]
+              when :right then [end_x * 0.1, end_y - (height / 2), end_x * 0.9, end_y - (height / 2), duration]
+              else
+                raise('Only upwards and downwards scrolling are supported')
+              end
+        gesture(loc)
+      end
+
+      def scroll_element_down
+        swipe_my_element(:down)
+      end
+
+      def scroll_element_up
+        swipe_my_element(:up)
+      end
+
+      def swipe_element_left
+        swipe_my_element(:left, 2000)
+      end
+
+      def swipe_element_right
+        swipe_my_element(:right, 2000)
       end
 
       def scroll_to_text(text)
@@ -146,6 +226,16 @@ module ScreenObject
           scroll_to_exact_text(text)
           element.click
         end
+      end
+
+      def has_text(text)
+        items = elements
+        items.each do |item|
+          text_value = item.attribute('text').strip
+          return item if text_value.casecmp?(text.strip.to_s)
+        end
+        msg = "Unable to find element with text: #{text}"
+        raise(msg)
       end
 
     end
